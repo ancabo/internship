a41ac1c4302680b79ef3ff0d45836bb048d2e354
anca <anca.bota@siemens.com>
2018-07-09 09:17:32 +0300

    add dependensies to pom and TestBase class

---
+++ src/main/java/common/TestBase.java
@@ -0,0 +1,26 @@
+package common;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+
+public class TestBase {
+	
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void setUpDriver() {
+		System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@AfterTest
+	public void quitDriver() {
+		if (driver != null)
+			driver.quit();
+	}
+	
+
+}
